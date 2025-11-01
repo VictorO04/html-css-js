761efaeaf8c3d67c7762a0237c982b285d64eb44
Victor Oliveira <victorfoliveira0407@gmail.com>
2025-11-01 16:35:56 -0300

    feat: termina trocar tema

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ const gif = document.getElementById("imagem");
 const btnSim = document.getElementById("btn-sim");
 const btnNao = document.getElementById("btn-nao");
 
+const btnTema = document.getElementById("tema");
+const body = document.body;
+
 btnSim.addEventListener("click", () => {
     titulo.textContent = "Parabéns, você ganhou 1000 de Aura";
     gif.src = "../assets/img/yujiaura.gif";
@@ -12,3 +15,13 @@ btnNao.addEventListener("click", () => {
     titulo.textContent = "Deplorável, você perder 1000 de Aura";
     gif.src = "../assets/img/yujidepre.gif";
 });
+
+btnTema.addEventListener("click", () => {
+    if (body.classList.contains("claro")) {
+        body.classList.replace("claro", "escuro");
+        btnTema.textContent = "☀️";
+    } else {
+        body.classList.replace("escuro", "claro");
+        btnTema.textContent = "🌕";
+    }
+})
